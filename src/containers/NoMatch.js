40dd2c9579114e594da277c25fbb79f5cebaa077
Nima Kaviyani <32512803+NimaKaviyani@users.noreply.github.com>
2020-11-26 00:30:00 +0330

    added 404 page

--- src/containers/NoMatch.js
+++ src/containers/NoMatch.js
@@ -1,11 +1,21 @@
-import React, {Component} from "react";
+import React, {Component} from 'react';
+import {Col, Container, Row} from 'react-bootstrap';
+import {Link} from 'react-router-dom';
+import PublicLayout from '../layout/PublicLayout';
 
 class NoMatch extends Component {
     render() {
         return (
-            <div>
-                <h1>Not Found</h1>
-            </div>
+            <PublicLayout>
+                <Col
+                    md={8}
+                    sm={10}
+                >
+                    <h1>404</h1>
+                    <p>Ooops... Error 404 We are sorry, but the page you are looking for does not exist.</p>
+                    <Link to={'/'}>Back to homepage</Link>
+                </Col>
+            </PublicLayout>
         );
     }
 }
