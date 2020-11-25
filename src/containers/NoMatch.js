a04a51428b98ae58547d0c05bd6b82ed386841b8
Nima Kaviyani <32512803+NimaKaviyani@users.noreply.github.com>
2020-11-26 00:30:24 +0330

    added 404 page

--- src/containers/NoMatch.js
+++ src/containers/NoMatch.js
@@ -1,5 +1,5 @@
 import React, {Component} from 'react';
-import {Col, Container, Row} from 'react-bootstrap';
+import {Col} from 'react-bootstrap';
 import {Link} from 'react-router-dom';
 import PublicLayout from '../layout/PublicLayout';
 
