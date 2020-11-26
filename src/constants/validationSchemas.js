f3baf78eb02474b28190374b9401800f16cdada8
Nima Kaviyani <32512803+NimaKaviyani@users.noreply.github.com>
2020-11-26 11:03:08 +0330

    added validationSchemas.js

---
+++ src/constants/validationSchemas.js
@@ -0,0 +1,29 @@
+import * as Yup from 'yup';
+
+export const SignInSchema = Yup.object().shape({
+    password:
+        Yup.string()
+           .min(7, 'Too Short!')
+           .max(50, 'Too Long!')
+           .required('Please enter your password'),
+    email:
+        Yup.string()
+           .email('Invalid email')
+           .required('Please enter your email address'),
+});
+
+export const SignUpSchema = Yup.object().shape({
+    password:
+        Yup.string()
+           .min(7, 'Too Short!')
+           .max(50, 'Too Long!')
+           .required('Please enter your password'),
+    repeat:
+        Yup.string()
+           .required('Please enter your repeat password')
+           .oneOf([Yup.ref('password'), null], 'Passwords must match'),
+    email:
+        Yup.string()
+           .email('Invalid email')
+           .required('Please enter your email address'),
+});
